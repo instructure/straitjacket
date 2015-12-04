60751cb471c40def9743beb64281fc7a0aeca3c2
Brian Palmer <brianp@instructure.com>
2015-12-04 09:15:30 -0700

    swagger api improvements
    Change-Id: I257eab09602873836751db62011dab6e11ca8cf0

--- run-dev.sh
+++ run-dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t straitjacket . && docker run -v /var/run/docker.sock:/var/run/docker.sock -v /tmp:/tmp --rm -p 8081:8081 straitjacket $@
+docker build -t straitjacket . && docker run -v /var/run/docker.sock:/var/run/docker.sock -v /tmp:/tmp -v $(pwd):/go/src/straitjacket --rm -p 8081:8081 straitjacket $@
